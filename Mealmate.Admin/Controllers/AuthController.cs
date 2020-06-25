3d7a55168bfca3a3eff438388f339cfcd21b861b
askhorizons <salman@askhorizons.com>
2020-06-25 23:29:49 +0500

    Auth and Home controller added
    Related Work Items: #33

---
+++ Mealmate.Admin/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mealmate.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+
+        }
+
+        [HttpGet()]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
