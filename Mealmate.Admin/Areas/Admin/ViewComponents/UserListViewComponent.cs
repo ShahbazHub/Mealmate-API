21eee7181b0ef6457d63b53cc5db9e2dff833dcf
askhorizons <salman@askhorizons.com>
2020-06-26 21:44:43 +0500

    Read, Create and update done
    Related Work Items: #40

--- Mealmate.Admin/Areas/Admin/ViewComponents/UserListViewComponent.cs
+++ Mealmate.Admin/Areas/Admin/ViewComponents/UserListViewComponent.cs
@@ -25,10 +25,33 @@ namespace Mealmate.Admin.Areas.Admin.ViewComponents
             {
                 new UserListViewModel()
                 {
+                    AppUserId = 1,
                     Email = "admin1234@gmail.com",
                     Name = "Muhammad Ali",
                     Phone = "",
-                    Username = "admin1234"
+                    Username = "admin1234",
+                    StateId = 1,
+                    Roles = 1
+                },
+                new UserListViewModel()
+                {
+                    AppUserId = 2,
+                    Email = "johndoe@gmail.com",
+                    Name = "John Doe",
+                    Phone = "",
+                    Username = "johndoe",
+                    StateId = 2,
+                    Roles = 1
+                },
+                new UserListViewModel()
+                {
+                    AppUserId = 2,
+                    Email = "tahirsaleem@gmail.com",
+                    Name = "Tahir Saleem",
+                    Phone = "",
+                    Username = "tahir1985",
+                    StateId = 1,
+                    Roles = 1
                 }
             };
 
