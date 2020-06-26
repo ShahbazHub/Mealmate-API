d06ccad6c8a7b1ce60fa5ab9b56457d76ef48ae1
askhorizons <salman@askhorizons.com>
2020-06-26 15:28:20 +0500

    1. Home page 2. Admin area
    Related Work Items: #33

--- Mealmate.Admin/Startup.cs
+++ Mealmate.Admin/Startup.cs
@@ -42,8 +42,14 @@ namespace Mealmate.Admin
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                
             });
         }
     }
