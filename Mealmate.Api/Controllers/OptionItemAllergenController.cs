81c639ab0b76689521946b29467ce82a35b3668e
askhorizons <salman@askhorizons.com>
2020-07-19 14:21:44 +0500

    option item allergen and dietaary api

---
+++ Mealmate.Api/Controllers/OptionItemAllergenController.cs
@@ -0,0 +1,102 @@
+﻿using Mealmate.Api.Helpers;
+using Mealmate.Api.Requests;
+using Mealmate.Application.Interfaces;
+using Mealmate.Application.Models;
+using Mealmate.Core.Paging;
+
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Mealmate.Api.Controllers
+{
+    [Route("api/optionItemAllergens")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class OptionItemAllergenController : ControllerBase
+    {
+        private readonly IOptionItemAllergenService _optionItemAllergenService;
+
+        public OptionItemAllergenController(
+            IOptionItemAllergenService optionItemAllergenService
+            )
+        {
+            _optionItemAllergenService = optionItemAllergenService ?? throw new ArgumentNullException(nameof(optionItemAllergenService));
+        }
+
+        #region Read
+        [Route("{optionItemId}")]
+        [HttpGet()]
+        [ProducesResponseType(typeof(IEnumerable<OptionItemAllergenModel>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OptionItemAllergenModel>>> Get(int optionItemId, [FromQuery] PageSearchArgs request)
+        {
+            try
+            {
+                var OptionItemAllergens = await _optionItemAllergenService.Search(optionItemId, request);
+                JToken _jtoken = TokenService.CreateJToken(OptionItemAllergens, request.Props);
+                return Ok(_jtoken);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [Route("single/{optionItemAllergenId}")]
+        [HttpGet()]
+        [ProducesResponseType(typeof(OptionItemAllergenModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OptionItemAllergenModel>> Get(int optionItemAllergenId)
+        {
+            try
+            {
+                var temp = await _optionItemAllergenService.Get(optionItemAllergenId);
+                return Ok(temp);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+        #region Create
+        [HttpPost()]
+        [ProducesResponseType(typeof(OptionItemAllergenModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<OptionItemAllergenModel>> Create([FromBody] OptionItemAllergenModel request)
+        {
+            var result = await _optionItemAllergenService.Create(request);
+            return Created($"api/optionitemallergens/{result.Id}", result);
+        }
+        #endregion
+
+        #region Update
+        [HttpPut()]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> Update([FromBody] OptionItemAllergenModel request)
+        {
+            await _optionItemAllergenService.Update(request);
+            return Ok();
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete("{optionItemAllergenId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> Delete(int optionItemAllergenId)
+        {
+            await _optionItemAllergenService.Delete(optionItemAllergenId);
+            return Ok();
+        }
+        #endregion
+
+    }
+}
---
+++ Mealmate.Api/Controllers/OptionItemDietaryController.cs
@@ -0,0 +1,102 @@
+﻿using Mealmate.Api.Helpers;
+using Mealmate.Api.Requests;
+using Mealmate.Application.Interfaces;
+using Mealmate.Application.Models;
+using Mealmate.Core.Paging;
+
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Mealmate.Api.Controllers
+{
+    [Route("api/optionItemDietaries")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class OptionItemDietaryController : ControllerBase
+    {
+        private readonly IOptionItemDietaryService _optionItemDietaryService;
+
+        public OptionItemDietaryController(
+            IOptionItemDietaryService optionItemDietaryService
+            )
+        {
+            _optionItemDietaryService = optionItemDietaryService ?? throw new ArgumentNullException(nameof(optionItemDietaryService));
+        }
+
+        #region Read
+        [Route("{optionItemId}")]
+        [HttpGet()]
+        [ProducesResponseType(typeof(IEnumerable<OptionItemDietaryModel>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OptionItemDietaryModel>>> Get(int optionItemId, [FromQuery] PageSearchArgs request)
+        {
+            try
+            {
+                var OptionItemDietarys = await _optionItemDietaryService.Search(optionItemId, request);
+                JToken _jtoken = TokenService.CreateJToken(OptionItemDietarys, request.Props);
+                return Ok(_jtoken);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [Route("single/{optionItemDietaryId}")]
+        [HttpGet()]
+        [ProducesResponseType(typeof(OptionItemDietaryModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OptionItemDietaryModel>> Get(int optionItemDietaryId)
+        {
+            try
+            {
+                var temp = await _optionItemDietaryService.Get(optionItemDietaryId);
+                return Ok(temp);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+
+        #region Create
+        [HttpPost()]
+        [ProducesResponseType(typeof(OptionItemDietaryModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<OptionItemDietaryModel>> Create([FromBody] OptionItemDietaryModel request)
+        {
+            var result = await _optionItemDietaryService.Create(request);
+            return Created($"api/optionitemdietaries/{result.Id}", result);
+        }
+        #endregion
+
+        #region Update
+        [HttpPut()]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> Update([FromBody] OptionItemDietaryModel request)
+        {
+            await _optionItemDietaryService.Update(request);
+            return Ok();
+        }
+        #endregion
+
+        #region Delete
+        [HttpDelete("{optionItemDietaryId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> Delete(int optionItemDietaryId)
+        {
+            await _optionItemDietaryService.Delete(optionItemDietaryId);
+            return Ok();
+        }
+        #endregion
+
+    }
+}
