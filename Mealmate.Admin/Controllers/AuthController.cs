094b6d7d004c5a5955a8edfa86e345817249e1b7
askhorizons <salman@askhorizons.com>
2020-07-29 22:16:20 +0500

    admin removed

---
+++ Mealmate.Admin/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mealmate.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        public AuthController()
+        {
+
+        }
+
+        [HttpGet()]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mealmate.Admin/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mealmate.Admin
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (_env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            // Matches request to an endpoint.
+            app.UseRouting();
+
+            // Endpoint aware middleware. 
+            // Middleware can use metadata from the matched endpoint.
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller}/{action}/{id?}",
+                   defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
