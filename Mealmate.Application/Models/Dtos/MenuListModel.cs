f1d71b70ea5794c976a0099af90e8f04b738214f
web <web@utorsols.com>
2020-08-29 12:27:45 -0700

    serviceTimeFrom and ServiceTimeTo added in menulistmodel.

--- Mealmate.Application/Models/Dtos/MenuListModel.cs
+++ Mealmate.Application/Models/Dtos/MenuListModel.cs
@@ -8,7 +8,8 @@ namespace Mealmate.Application.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public TimeSpan ServiceTime { get; set; }
+        public TimeSpan ServiceTimeFrom { get; set; }
+        public TimeSpan ServiceTimeTo { get; set; }
         public List<MenuItemListModel> MenuItems { get; set; }
         public MenuListModel()
         {
